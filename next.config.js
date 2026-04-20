5feca4f92fd3b72003f99ee433b6ed838d67047b
mantrasite <mantrasito@gmail.com>
2026-04-20 22:09:52 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
     unoptimized: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
