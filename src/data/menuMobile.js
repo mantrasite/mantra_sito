8bc658bcf72565255c7b5e80dd4e4c650749169b
mantrasite <mantrasito@gmail.com>
2026-04-24 14:27:36 +0200

    Delete src/data/menuMobile.js

--- src/data/menuMobile.js
+++
@@ -1,5 +0,0 @@
-import data from "./menuMobile.json";
-
-export const restaurantInfo = data.restaurantInfo;
-export const allergensInfo = data.allergensInfo;
-export const menuSections = data.menuSections;
