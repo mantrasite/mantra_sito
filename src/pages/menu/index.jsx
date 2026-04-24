07886688b2d5c76b9e80d8bd79075d6ab43dfcba
mantrasite <mantrasito@gmail.com>
2026-04-24 14:28:16 +0200

    Update index.jsx

--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -1,10 +1,9 @@
 import MenuExperience from "@/components/MenuExperience";
-import { menuSections, restaurantInfo } from "@/data/menuMobile";
 
 export default function MenuPage() {
   return (
     <div className="w-screen h-full">
-      <MenuExperience sections={menuSections} restaurant={restaurantInfo} />
+      <MenuExperience />
     </div>
   );
 }
